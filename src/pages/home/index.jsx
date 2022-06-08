2fd974a1a01ff39ff1e13d2db5fb69992d93257c
yikang <yikang@cht.com.tw>
2022-06-08 14:39:41 +0800

    second commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,179 +1,133 @@
-import React from 'react';
-import { Layout, Menu } from 'antd';
 import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
+    BarChartOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    TeamOutlined,
+    FormOutlined,
 } from '@ant-design/icons';
-import './index.css'
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import './index.css';
+import chtLogo from './CHT.png';
+import chtLogo32x32 from './CHT_32x32.png';
+import chtFooter from './CHTFooter.png';
 
-const { Header, Content, Footer, Sider } = Layout;
-const items = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const { Header, Sider, Content, Footer } = Layout;
 
-const Home = () => (
-  <Layout hasSider>
-    <Sider
-      style={{
-        overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',
-        left: 0,
-        top: 0,
-        bottom: 0,
-      }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-    </Sider>
-    <Layout
-      className="site-layout"
-      style={{
-        marginLeft: 200,
-      }}
-    >
-      <Header
-        className="site-layout-background"
-        style={{
-          padding: 0,
-        }}
-      />
-      <Content
-        style={{
-          margin: '24px 16px 0',
-          overflow: 'initial',
-        }}
-      >
-        <div
-          className="site-layout-background"
-          style={{
-            padding: 24,
-            textAlign: 'center',
-          }}
-        >
-          ...
-          <br />
-          Really
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          long
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          ...
-          <br />
-          content
-        </div>
-      </Content>
-      <Footer
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
-    </Layout>
-  </Layout>
-);
+const Home = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Layout id="components-layout-demo-custom-trigger">
+            <Sider trigger={null} collapsible collapsed={collapsed} style={{ backgroundColor: "white" }} >
+                {
+                    collapsed ?
+                        <img className="logo_min" src={chtLogo32x32} alt="中華電信" /> :
+                        <img className="logo_full" src={chtLogo} alt="中華電信" />
+                }
+                <Menu
+                    theme="light"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <BarChartOutlined />,
+                            label: '系統儀表板',
+                        },
+                        {
+                            key: '2',
+                            icon: <UserOutlined />,
+                            label: '帳號管理',
+                        },
+                        {
+                            key: '3',
+                            icon: <TeamOutlined />,
+                            label: '人員管理',
+                        },
+                        {
+                            key: 'device',
+                            icon: <VideoCameraOutlined />,
+                            label: '裝置管理',
+                            children: [
+                                {
+                                    key: '4',
+                                    label: '裝置列表'
+                                },
+                                {
+                                    key: '5',
+                                    label: '裝置儀表板'
+                                }
+                            ]
+                        },
+                        {
+                            key: 'record',
+                            icon: <FormOutlined />,
+                            label: '紀錄管理',
+                            children: [
+                                {
+                                    key: '6',
+                                    label: '即時辨識'
+                                },
+                                {
+                                    key: '7',
+                                    label: '辨識紀錄'
+                                },
+                                {
+                                    key: '8',
+                                    label: '操作紀錄'
+                                },
+                                {
+                                    key: '9',
+                                    label: '出勤紀錄'
+                                },
+                                {
+                                    key: '10',
+                                    label: '出勤時數'
+                                }
+                            ]
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                >
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px 0',
+                        overflow: 'initial',
+                        // padding: 24,
+                        // minHeight: 280,
+                    }}
+                >
+                    Content
+                </Content>
+                <Footer
+                    style={{
+                        margin: '0px 16px',
+                        textAlign: "center",
+                        backgroundImage: `url(${chtFooter})`,
+                        backgroundRepeat: "repeat-x",
+                        backgroundSize: "contain"
+                    }}
+                >
+                    中華電信股份有限公司版權所有 ©{new Date().getFullYear()}  Chunghwa Telecom Co., Ltd. All Rights Reserved.
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+}
 
 export default Home;
--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -1,22 +1,119 @@
-import React from 'react';
-import { Layout } from 'antd';
-import Login from '../../components/Login'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Layout, Form, Input, Button } from 'antd';
 import 'antd/dist/antd.min.css';
+import loginBG from './LoginBG.png';
+import logInLogo from './logInLogo.png';
 
-const { Header, Footer, Content } = Layout;
+const { Content, Footer } = Layout;
 
 export default function Signin() {
+
+  const [account, setAccount] = useState('');
+  const [pwd, setPwd] = useState('');
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    if (account === 'admin' && pwd === 'hiserver') {
+      navigate('/home')
+    } else {
+      navigate('/signin')
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    alert('帳密不全')
+  };
+
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Layout>
-        <Header style={{ color: 'white' }}>
-          HiFace人臉辨識系統
-        </Header>
-        <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Login />
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>中華電信股份有限公司版權所有 ©2022  Chunghwa Telecom Co., Ltd. All Rights Reserved.</Footer>
-      </Layout>
+    <Layout style={{
+      height: "100vh",
+      width: "100vw"
+    }}>
+      {/* css參考設定:https://adanaa.com/css-div-div */}
+      <Content className='container' style={{
+        position: "relative",
+        overflow: "hidden"
+      }}>
+        <img className='content' src={loginBG} alt="login_background" style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          display: "block",
+          minHeight: "100%",
+          minWidth: "100%",
+          transform: "translate(-50%,-50%)",
+          opacity: "0.6"
+        }} />
+        <div style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          display: "flex",
+          flexDirection: 'column',
+          justifyContent:'center',
+          backgroundColor: "#f0f2f5",
+          borderRadius: "10px",
+          width: "350px",
+          height: "450px",
+          transform: "translate(-50%,-50%)",
+          padding: "20px"
+        }}>
+          <img src={logInLogo} alt="chtIVS" style={{ width: "100%", margin: "0 0 24px", padding:"0 10px" }}></img>
+          <Form
+            name="loginForm"
+            labelCol={{
+              span: 24
+            }}
+            wrapperCol={{
+              spna: 24
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="帳號"
+              name="account"
+              rules={[
+                {
+                  required: true,
+                  message: "請輸入帳號！",
+                }
+              ]}>
+              <Input onChange={(event) => { setAccount(event.target.value) }} placeholder='Account' />
+            </Form.Item>
+
+            <Form.Item
+              label="密碼"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "請輸入密碼！",
+                }
+              ]}>
+              <Input.Password onChange={(event) => { setPwd(event.target.value) }} placeholder='Password' />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                登入
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+      <Footer style={{
+        textAlign: 'center'
+      }}>中華電信股份有限公司版權所有 ©{new Date().getFullYear()}  Chunghwa Telecom Co., Ltd. All Rights Reserved.</Footer>
     </Layout>
   )
 }
+
+// style={{
+//   backgroundImage: `url(${loginBG})`,
+//   backgroundRepeat: "no-repeat",
+//   backgroundSize: "contain",
+//   opacity: "0.8",
+// }}
